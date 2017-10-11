cf011755d4619c6da336de10abf9d498fb481d46
Orestis <orestis.zambounis@nextron.ch>
2017-10-11 17:32:29 +0200

    removed unused files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,5 @@
 import mineImage from './mine.png';
 import flagImage from './flag.png';
-import flagShadeImage from './flagShade.png';
 import crossImage from './cross.png';
 import smileImage from './smile.png';
 import neutralImage from './neutral.png';
@@ -10,7 +9,6 @@ import sunglassesImage from './sunglasses.png';
 export {
   crossImage,
   flagImage,
-  flagShadeImage,
   mineImage,
   smileImage,
   neutralImage,
